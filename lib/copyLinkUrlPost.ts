f71c107d523a019b57c57c5216149c6c82083b79
sosse-dev <sosse3135@gmail.com>
2024-05-15 13:12:30 +0800

    #

--- lib/copyLinkUrlPost.ts
+++ lib/copyLinkUrlPost.ts
@@ -1,5 +1,5 @@
 export const copyLinkUrlPost = (postId: string) => {
-  const base = "http://localhost:3000";
+  const base = process.env.NEXT_URL;
   // TODO: Ganti kalau udah deploy!
 
   const link = `${base}/post?id=${postId}`;
